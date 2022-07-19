993a3cbd503b6cc360ba22f158dc047de08d3eb1
jiang-xia <963798512@qq.com>
2022-07-19 18:01:22 +0800

    feat:  修复项目直接引用jsx或tsx报错原因

---
+++ .husky/note.md
@@ -0,0 +1,12 @@
+常用的type类别
+
+upd：更新某功能（不是 feat, 不是 fix）
+feat：新功能（feature）
+fix：修补bug
+docs：文档（documentation）
+style： 格式（不影响代码运行的变动）
+refactor：重构（即不是新增功能，也不是修改bug的代码变动）
+test：增加测试
+chore：构建过程或辅助工具的变动
+
+示例：feat(add):  1.添加新功能
--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
-  import { reactive, ref } from 'vue';
+  import XMarkdownReader from '@/components/x-markdown-reader';
+
+  const contentHtml =
+    '<h2 id="spwwn">江夏</h2><blockquote><p><font size="2">一位来自广州的前端程序员 喜欢看电影，玩游戏，学技术。</font><br/></p></blockquote>';
 </script>
 
-<template> <div>user</div></template>
+<template>
+  <div> <XMarkdownReader :content="contentHtml" /> </div
+></template>
 
 <style lang="less" scoped></style>
