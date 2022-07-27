b1f161a259ff84594bbacaf967f7f6c479e9bb22
jiang-xia <963798512@qq.com>
2022-07-27 18:26:42 +0800

    fix:  url不使用环境配置的

--- src/api/interceptor.ts
+++ src/api/interceptor.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { Message, Modal } from '@arco-design/web-vue';
 import { useUserStore } from '@/store';
 import { getToken } from '@/utils/auth';
-// import { baseUrl } from '@/config';
+import { baseUrl } from '@/config';
 
 // 自定义请求和相应拦截器
 export interface HttpResponse<T = unknown> {
@@ -16,7 +16,8 @@ function errorMsg(msg: string) {
 }
 // 这是baseUrl使用 环境模式中写的变量  或者也可以使用config index中配置地址
 if (import.meta.env.VITE_API_BASE_URL) {
-  axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+  // axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+  axios.defaults.baseURL = baseUrl; // 不用重启vite
 }
 
 axios.interceptors.request.use(
